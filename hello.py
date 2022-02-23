de0cd52bdb041c738c27b5e7ea3822b8d72eb9cc
tangkete1 <15916413444@139.com>
2022-02-23 23:29:53 +0800

    调试无头访问

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+import os
 print("hello world")
 print("test-dev update!!")
-for i in range(1,10):
-    print(i)
-
-print("不用链接git更新代码，")
+# for i in range(1,10):
+#     print(i)
+DIR_PATH = os.path.abspath(os.path.abspath(__file__))
+print(DIR_PATH)
+print(os.path.dirname(__file__))
--- openBrowser.py
+++ openBrowser.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 from time import sleep
 from selenium import webdriver
-ChromeOptions = webdriver.ChromeOptions()
-ChromeOptions.add_argument('--headless')
-ChromeOptions.add_argument('--no-sandbox')
-ChromeOptions.add_argument('--disable-gpu')
-ChromeOptions.add_argument('--disable-dev-shm-usage')
-ChromeOptions.add_argument('window-size=1200x600')
+# ChromeOptions = webdriver.ChromeOptions()
+# ChromeOptions.add_argument('--headless')
+# ChromeOptions.add_argument('--no-sandbox')
+# ChromeOptions.add_argument('--disable-gpu')
+# ChromeOptions.add_argument('--disable-dev-shm-usage')
+# ChromeOptions.add_argument('window-size=1200x600')
+from Options.Chrome_options import ChromeOptions
+sys.path.append(os.path.dirname(__file__))
 
-driver = webdriver.Chrome(options=ChromeOptions)
+driver = webdriver.Chrome(options=ChromeOptions().options())
 driver.implicitly_wait(10)
 driver.get('https://www.baidu.com')
 driver.find_element('id','kw').send_keys('xuzhu')
 driver.find_element('id','su').click()
 sleep(3)
 driver.quit()
+
