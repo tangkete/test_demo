0d317ded0ffb6b5459d46a28082b8d4e5c876567
tangkete1 <15916413444@139.com>
2022-02-23 22:42:42 +0800

    提交第一个脚本

--- openBrowser.py
+++ openBrowser.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from time import sleep
 from selenium import webdriver
-driver = webdriver.Chrome()
+from Options.Chrome_options import ChromeOptions
+
+driver = webdriver.Chrome(options=ChromeOptions().options())
+driver.implicitly_wait(10)
 driver.get('https://www.baidu.com')
+driver.find_element('id','kw').send_keys('xuzhu')
+driver.find_element('id','su').click()
 sleep(3)
 driver.quit()
