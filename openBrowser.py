036d02b3a8d9842e443ce76c1ab4e72d98e8579d
tangkete1 <15916413444@139.com>
2022-02-23 23:07:14 +0800

    调试无头访问

--- openBrowser.py
+++ openBrowser.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from time import sleep
 from selenium import webdriver
-from Options.Chrome_options import ChromeOptions
+ChromeOptions = webdriver.ChromeOptions()
+ChromeOptions.add_argument('--headless')
+ChromeOptions.add_argument('--no-sandbox')
+ChromeOptions.add_argument('--disable-gpu')
+ChromeOptions.add_argument('--disable-dev-shm-usage')
+ChromeOptions.add_argument('window-size=1200x600')
 
-driver = webdriver.Chrome(options=ChromeOptions().options())
+driver = webdriver.Chrome(options=ChromeOptions)
 driver.implicitly_wait(10)
 driver.get('https://www.baidu.com')
 driver.find_element('id','kw').send_keys('xuzhu')
